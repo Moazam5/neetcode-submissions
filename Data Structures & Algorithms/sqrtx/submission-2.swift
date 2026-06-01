0192dfa4eadd4ba213d4d1300db5680553f68b59
Moazam Mir <37161391+Moazam5@users.noreply.github.com>
2026-06-01 15:31:09 -0700

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        var l = 0 
+        var r = x
+
+        while l <= r {
+            let m = l + (r - l)  / 2
+            if m * m == x {
+                return m 
+            } else if (m * m) > x {
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+        return l - 1
+    }
+}
