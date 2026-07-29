fd2ebd57c86cde9154aade9b942518603e73facb
Moazam Mir <37161391+Moazam5@users.noreply.github.com>
2026-07-29 12:22:49 -0700

    Add: delete-leaves-with-a-given-value - submission-0

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-0.swift
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func removeLeafNodes(_ root: TreeNode?, _ target: Int) -> TreeNode? {
+        guard let root else {
+            return nil 
+        }
+        root.left = removeLeafNodes(root.left, target)
+        root.right = removeLeafNodes(root.right, target)
+
+        if root.left == nil && root.right == nil && root.val == target {
+            return nil
+        }
+        return root
+
+    }
+}
