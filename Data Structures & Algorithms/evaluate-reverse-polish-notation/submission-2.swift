a57e382e14458c2d39c0313f60088dc5d2ab364f
Moazam Mir <37161391+Moazam5@users.noreply.github.com>
2026-08-02 13:17:29 -0700

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.swift
@@ -0,0 +1,33 @@
+class Solution {
+    func evalRPN(_ tokens: [String]) -> Int {
+        var stack = [Int]() {
+            didSet {
+                print(stack)
+            }
+        }
+
+        for token in tokens { 
+            switch token {
+                case "+":
+                    let a = stack.removeLast()
+                    let b = stack .removeLast()
+                    stack.append(a + b)
+                case "-":
+                    let a = stack.removeLast()
+                    let b = stack .removeLast()
+                    stack.append(b - a)
+                case "*":
+                    let a = stack.removeLast()
+                    let b = stack .removeLast()
+                    stack.append(a * b)
+                case "/":
+                    let a = stack.removeLast()
+                    let b = stack .removeLast()
+                    stack.append(b / a)
+                default: 
+                    stack.append(Int(token)!)
+            }
+        }
+        return stack.last ?? 0
+    }
+}
